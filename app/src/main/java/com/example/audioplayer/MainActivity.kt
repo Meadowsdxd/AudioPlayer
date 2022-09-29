3bc124f4f15180307852b38cf76f54d7ca8693cb
Meadowsdxd <58809634+Meadowsdxd@users.noreply.github.com>
2022-09-29 20:25:11 +0300

    add method for transfer between forms

--- app/src/main/java/com/example/audioplayer/MainActivity.kt
+++ app/src/main/java/com/example/audioplayer/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.audioplayer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.audioplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.shuffleBTN.setOnClickListener{
+            val intent= Intent(this@MainActivity,activity_player::class.java)
+            startActivity(intent)
+        }
+        binding.favoriteBTN.setOnClickListener{
+            val intent= Intent(this@MainActivity,Favorite::class.java)
+            startActivity(intent)
+        }
+        binding.playlistBTN.setOnClickListener{
+            val intent= Intent(this@MainActivity,PlaylistActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
