adf31993aa1ac4135384f9933a5a9d2cb94e48f2
Meadowsdxd <58809634+Meadowsdxd@users.noreply.github.com>
2022-10-02 20:02:05 +0300

    add RecyclerView

--- app/src/main/java/com/example/audioplayer/MainActivity.kt
+++ app/src/main/java/com/example/audioplayer/MainActivity.kt
@@ -8,6 +8,7 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.app.ActivityCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.audioplayer.databinding.ActivityMainBinding
 import kotlin.system.exitProcess
 
@@ -15,18 +16,12 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private  lateinit var toggle: ActionBarDrawerToggle
+    private  lateinit var musicAdapter: MusicAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTheme(R.style.Theme_AudioPlayer)
-        requestRuntimePermission()
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        toggle= ActionBarDrawerToggle(this,binding.root,R.string.open,R.string.close)
-        binding.root.addDrawerListener(toggle)
-        toggle.syncState()
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
+        init()
 
         binding.shuffleBTN.setOnClickListener{
              startActivity( Intent(this@MainActivity,activity_player::class.java))
@@ -73,5 +68,25 @@ class MainActivity : AppCompatActivity() {
        if(toggle.onOptionsItemSelected(item))
             return true
         return super.onOptionsItemSelected(item)
+    }
+    private  fun init(){
+        setTheme(R.style.Theme_AudioPlayer)
+        requestRuntimePermission()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val musicList= ArrayList<String>()
+        musicList.add("1 song")
+        musicList.add("2 song")
+        musicList.add("3 song")
+        musicList.add("4 song")
+        toggle= ActionBarDrawerToggle(this,binding.root,R.string.open,R.string.close)
+        binding.root.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.musicRecycler.setHasFixedSize(true)
+        binding.musicRecycler.setItemViewCacheSize(13)
+        binding.musicRecycler.layoutManager=LinearLayoutManager(this@MainActivity,)
+        musicAdapter= MusicAdapter(this@MainActivity,musicList)
+        binding.musicRecycler.adapter=musicAdapter
     }
 }
---
+++ app/src/main/java/com/example/audioplayer/MusicAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.audioplayer
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+import com.example.audioplayer.databinding.MusicviewBinding
+
+class MusicAdapter(private val  context:Context,private val  musicList:ArrayList<String>): RecyclerView.Adapter<MusicAdapter.MyHolder>() {
+
+    class MyHolder(binding: MusicviewBinding):RecyclerView.ViewHolder(binding.root) {
+        val title=binding.songNameMV
+        val album=binding.songAlbumMV
+        val image=binding.imageView
+        val duration=binding.songDuration
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MusicAdapter.MyHolder {
+        return MyHolder(MusicviewBinding.inflate(LayoutInflater.from(context),parent,false))
+    }
+
+    override fun onBindViewHolder(holder: MusicAdapter.MyHolder, position: Int) {
+        holder.title.text=musicList[position]
+    }
+
+    override fun getItemCount(): Int {
+       return musicList.size
+    }
+}
