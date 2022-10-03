56b317b600c0c863c10f3d6bf9e5dc10e67795a2
Meadowsdxd <58809634+Meadowsdxd@users.noreply.github.com>
2022-10-03 18:46:51 +0300

    add click on songs and do param in minutes

--- app/src/main/java/com/example/audioplayer/MusicAdapter.kt
+++ app/src/main/java/com/example/audioplayer/MusicAdapter.kt
@@ -1,8 +1,11 @@
 package com.example.audioplayer
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.util.TimeUtils.formatDuration
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -16,19 +19,25 @@ class MusicAdapter(private val  context:Context,private val  musicList:ArrayList
         val album=binding.songAlbumMV
         val image=binding.imageView
         val duration=binding.songDuration
+        val root =binding.root
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MusicAdapter.MyHolder {
         return MyHolder(MusicviewBinding.inflate(LayoutInflater.from(context),parent,false))
     }
 
-    override fun onBindViewHolder(holder: MusicAdapter.MyHolder, position: Int) {
+    override fun onBindViewHolder(holder: MyHolder, position: Int) {
+
         holder.title.text=musicList[position].title
         holder.album.text=musicList[position].album
-        holder.duration.text=musicList[position].duration.toString()
+        holder.duration.text= formatDuration(musicList[position].duration)
         Glide.with(context).load(musicList[position].artURI).apply(RequestOptions()
             .placeholder(R.drawable.ic_music).centerCrop())
             .into(holder.image)
+        holder.root.setOnClickListener{
+            val intent=Intent(context,activity_player::class.java)
+            ContextCompat.startActivity(context,intent,null)
+        }
     }
 
     override fun getItemCount(): Int {
