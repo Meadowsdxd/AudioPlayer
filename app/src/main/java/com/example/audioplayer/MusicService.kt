055c9c2f7dfbf2cf1188b01d4233d3656d46606e
Meadowsdxd <58809634+Meadowsdxd@users.noreply.github.com>
2022-10-08 19:34:02 +0300

    fix problem with notification

--- app/src/main/java/com/example/audioplayer/MusicService.kt
+++ app/src/main/java/com/example/audioplayer/MusicService.kt
@@ -29,46 +29,21 @@ class MusicService: Service() {
     }
     }
     fun showNotification(){
-        val intent = Intent(baseContext, MainActivity::class.java)
 
- /*       val contentIntent = PendingIntent.getActivity(this, 0, intent, 0)
-
-        val notification = androidx.core.app.NotificationCompat.Builder(baseContext, AppClass.CHANNEL_ID)
-            .setContentIntent(contentIntent)
+        var notification = NotificationCompat.Builder(baseContext, AppClass.CHANNEL_ID)
             .setContentTitle(activity_player.musicListPA[activity_player.songPosition].title)
-            .setContentText(activity_player.musicListPA[activity_player.songPosition].artist)
             .setSmallIcon(R.drawable.ic_music)
+            .setContentText((activity_player.musicListPA[activity_player.songPosition].artist))
             .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_launcher_foreground))
-            .setStyle(androidx.media.app.NotificationCompat.MediaStyle().setMediaSession(mediaSession.sessionToken))
-            .setPriority(androidx.core.app.NotificationCompat.PRIORITY_HIGH)
-            .setVisibility(androidx.core.app.NotificationCompat.VISIBILITY_PUBLIC)
+            .setStyle(androidx.media.app.NotificationCompat.MediaStyle())
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
             .setOnlyAlertOnce(true)
-            .addAction(R.drawable.ic_back, "Previous", null)
+            .addAction(R.drawable.ic_back, "Previous",null )
             .addAction(R.drawable.ic_pause, "Play", null)
             .addAction(R.drawable.ic_next, "Next", null)
             .addAction(R.drawable.ic_exit, "Exit", null)
             .build()
-      */
-        val pendingIntent: PendingIntent =
-            Intent(this, activity_player::class.java).let { notificationIntent ->
-                PendingIntent.getActivity(this, 0, notificationIntent,
-                    PendingIntent.FLAG_IMMUTABLE)
-            }
-
-        val notification: Notification = Notification.Builder(this, AppClass.CHANNEL_ID)
-            .setContentTitle(activity_player.musicListPA[activity_player.songPosition].title)
-            .setContentText(activity_player.musicListPA[activity_player.songPosition].artist)
-            .setSmallIcon(R.drawable.ic_music)
-            .setContentIntent(pendingIntent)
-            .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_launcher_foreground))
-            .setPriority(Notification.PRIORITY_HIGH)
-            .setVisibility(Notification.VISIBILITY_PUBLIC)
-            .setOnlyAlertOnce(true)
-            .addAction(R.drawable.ic_back, "Previous", null)
-            .addAction(R.drawable.ic_pause, "Play", null)
-            .addAction(R.drawable.ic_next, "Next", null)
-            .addAction(R.drawable.ic_exit, "Exit", null)
-            .build()
-        startForeground(13, notification)
+           startForeground(13, notification)
     }
 }
