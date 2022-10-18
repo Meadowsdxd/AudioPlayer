8dd11ee47e8a99f5c943a912e40f45799a45b7ea
Meadowsdxd <58809634+Meadowsdxd@users.noreply.github.com>
2022-10-18 21:45:05 +0300

    Done adapter in favorite view

--- app/src/main/java/com/example/audioplayer/Favorite.kt
+++ app/src/main/java/com/example/audioplayer/Favorite.kt
@@ -2,18 +2,31 @@ package com.example.audioplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.audioplayer.databinding.ActivityFavoriteBinding
 import com.example.audioplayer.databinding.ActivityMainBinding
 
 class Favorite : AppCompatActivity() {
 
     private lateinit var binding: ActivityFavoriteBinding
-
+    private lateinit var adapter: FavoriteAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.coolPink)
         binding = ActivityFavoriteBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val templist= ArrayList<String>()
+        templist.add("Song 1")
+        templist.add("Song 2")
+        templist.add("Song 3")
+        templist.add("Song 4")
         binding.backBTNPFA.setOnClickListener { finish() }
+        binding.favoriteRV.setHasFixedSize(true)
+        binding.favoriteRV.setItemViewCacheSize(13)
+        binding.favoriteRV.layoutManager= LinearLayoutManager(this)
+        binding.favoriteRV.layoutManager=GridLayoutManager(this,4)
+        adapter= FavoriteAdapter(this, templist)
+        binding.favoriteRV.adapter=adapter
     }
 }
---
+++ app/src/main/java/com/example/audioplayer/FavoriteAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.audioplayer
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.util.TimeUtils.formatDuration
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.audioplayer.databinding.ActivityFavoriteBinding
+import com.example.audioplayer.databinding.FavoriteViewBinding
+
+import com.example.audioplayer.databinding.MusicviewBinding
+
+class FavoriteAdapter(private val  context:Context,private var  musicList:ArrayList<String>): RecyclerView.Adapter<FavoriteAdapter.MyHolder>() {
+
+    class MyHolder(binding: FavoriteViewBinding):RecyclerView.ViewHolder(binding.root) {
+        val image=binding.songImgFV
+        val name=binding.songNameFV
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoriteAdapter.MyHolder {
+        return MyHolder(FavoriteViewBinding.inflate(LayoutInflater.from(context),parent,false))
+    }
+
+    override fun onBindViewHolder(holder: MyHolder, position: Int) {
+    holder.name.text=musicList[position]
+    }
+
+    override fun getItemCount(): Int {
+        return musicList.size
+    }
+
+    private fun sendIntent(ref:String,pos: Int){
+        val intent=Intent(context,activity_player::class.java)
+        intent.putExtra("index",pos)
+        intent.putExtra("class",ref)
+        ContextCompat.startActivity(context,intent,null)
+    }
+}
