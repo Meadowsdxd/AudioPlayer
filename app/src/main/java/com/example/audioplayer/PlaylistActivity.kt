09848891f6e95298fd594d82d0696f162c0b02f7
Meadowsdxd <58809634+Meadowsdxd@users.noreply.github.com>
2022-10-20 22:04:16 +0300

    Created PlaylistViewAdapter Class

--- app/src/main/java/com/example/audioplayer/PlaylistActivity.kt
+++ app/src/main/java/com/example/audioplayer/PlaylistActivity.kt
@@ -2,16 +2,29 @@ package com.example.audioplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.audioplayer.databinding.ActivityMainBinding
 import com.example.audioplayer.databinding.ActivityPlaylistBinding
 
 class PlaylistActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPlaylistBinding
-
+   private lateinit var adapter: PlaylistViewAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPlaylistBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val temp=ArrayList<String>()
+        temp.add("1 Songs")
+        temp.add("2 Songs")
+        temp.add("3 Songs")
+        binding.playlistRV.setHasFixedSize(true)
+        binding.playlistRV.setItemViewCacheSize(13)
+        binding.playlistRV.layoutManager= GridLayoutManager(this@PlaylistActivity,2)
+        adapter= PlaylistViewAdapter(this, temp)
+        binding.playlistRV.adapter=adapter
         binding.backBTNPLA.setOnClickListener { finish() }
+
     }
 }
